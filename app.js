f4dd7002c02eadb4eae6c16617dfaef4dced2418
gitcore33 <erickestanislau25@gmail.com>
2023-01-05 01:07:41 -0300

    rotas criadas

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // importar o express
 const express = require("express")
+const path = require("path")
 
 // criar o servidor
 const servidor = express()
+
+servidor.use(express.static(path.join(__dirname, 'public')))
 
 // definir uma rota neste servidor
 // endereço, método, função callback e a ação que o servidor vai realizar quando o servidor chegar
@@ -11,6 +14,53 @@ servidor.get('/usuarios', (req, res)=>{
     return res.send("Permaneça em linha!")
 })
 
+servidor.get('/carrinho', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'carrinho.html'))
+})
+
+servidor.get('/login', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'login.html'))
+})
+
+servidor.get('/criar-pizza', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'create-pizza.html'))
+})
+
+servidor.get('/seus-enderecos', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'enderecos.html'))
+})
+
+servidor.get('/404', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'error.html'))
+})
+
+servidor.get('/home', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'index.html'))
+})
+
+servidor.get('/lista-pizzas', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'list-pizza.html'))
+})
+
+servidor.get('/lista-usuarios', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'list.html'))
+})
+
+servidor.get('/login', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'login.html'))
+})
+
+servidor.get('/pedidos', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'pedidos.html'))
+})
+
+servidor.get('/perfil', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'perfil.html'))
+})
+
+servidor.get('/pizza', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'src', 'views', 'pizza.html'))
+})
 
 // por o servidor no modo aguardando requisição
 servidor.listen(3000)
